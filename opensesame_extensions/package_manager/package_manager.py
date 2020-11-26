31cb5038443f39d3817c9a3d36d98e754397c94f
smathot <s.mathot@cogsci.nl>
2020-11-26 16:02:11 +0100

    Fix spelling

--- opensesame_extensions/package_manager/package_manager.py
+++ opensesame_extensions/package_manager/package_manager.py
@@ -7,13 +7,13 @@ _ = translation_context(u'package_manager', category=u'extension')
 
 
 class package_manager(base_extension):
-	
+
 	def event_startup(self):
-		
+
 		self._initialized = False
-				
+
 	def init(self):
-		
+
 		from qtpip._qpipwidget import QPipWidget
 		self._qpipwidget = QPipWidget()
 		self._qpipwidget.log = self._log
@@ -25,7 +25,7 @@ class package_manager(base_extension):
 			'notify', message='Uninstalled %s' % pkg))
 		self._qpipwidget.install_failed.connect(
 			lambda pkg: self.extension_manager.fire(
-			'notify', message='Failted to installed %s' % pkg,
+			'notify', message='Failed to installed %s' % pkg,
 			category='warning'))
 		self._qpipwidget.uninstall_failed.connect(
 			lambda pkg: self.extension_manager.fire(
@@ -33,13 +33,13 @@ class package_manager(base_extension):
 			category='warning'))
 		self._initialized = True
 
-	def activate(self):		
-		
+	def activate(self):
+
 		if not self._initialized:
 			self.init()
 		self.tabwidget.add(self._qpipwidget, u'system-software-install',
 			_(u'Python package manager'))
-			
+
 	def _log(self, msg):
-		
+
 		self.console.write(msg + u'\n')
